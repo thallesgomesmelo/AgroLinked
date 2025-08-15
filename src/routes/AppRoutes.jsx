6235059082cd3fff8686741b077979fb0302e87c
unknown <thallesgomesmello@gmail.com>
2025-08-15 11:08:03 -0300

    Criado tela fale conosco.

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "../pages/Product/Product";
+import TalkUs from "../pages/TalkUs/TalkUs";
 import MainLayout from "../layouts/MainLayout";
 
 export default function AppRoutes() {
-    return (
-        <BrowserRouter>
-            <MainLayout>
-                <Routes>
-                    <Route path="/product" element={<Product />} />
-                </Routes>
-            </MainLayout>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <MainLayout>
+        <Routes>
+          <Route path="/product" element={<Product />} />
+          <Route path="/talkus" element={<TalkUs />} />
+        </Routes>
+      </MainLayout>
+    </BrowserRouter>
+  );
 }
