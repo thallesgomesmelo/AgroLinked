0b30d54f1a7559e9df630d51a3a066dd98ad15e3
jpvgoes <jpvgoes@gmail.com>
2025-08-17 21:50:52 -0300

    feita a componentização em 'src\components\Beneficios' da pagina beneficios e formatei o projeto inteiro alem de ajeitar algumas coisas que eu vi :)

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,51 +1,56 @@
-import '../../index.css'
-import '../../mediaquery.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import logoverde from "../../assets/logoverde.jpg";
+import "../../index.css";
+import "../../mediaquery.css";
 
 export default function Header() {
     return (
-        <>
-            <link
-                rel="stylesheet"
-                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-            />
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-            <link
-                href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
-                rel="stylesheet"
-            />
-            <header class="header">
-                <div class="container">
-                    <div class="header-content">
-                        <a href="index.html" class="logo">
-                            <img
-                                src="src\assets\logoverde.jpg"
-                                alt="AgroLinked Logo"
-                                width="40"
-                                height="40"
-                            />
-                            <span>AgroLinked</span>
-                        </a>
+        <header className="header">
+            <div className="container">
+                <div className="header-content">
+                    <Link to="/" className="logo">
+                        <img
+                            src={logoverde}
+                            alt="AgroLinked Logo"
+                            width="40"
+                            height="40"
+                        />
+                        <span>AgroLinked</span>
+                    </Link>
 
-                        <nav class="main-nav">
-                            <ul>
-                                <li><a href="index.html">Início</a></li>
-                                <li><a href="produto.html" class="active">Produto</a></li>
-                                <li><a href="beneficios.html">Benefícios</a></li>
-                                <li><a href="sobre.html">Sobre Nós</a></li>
-                                <li><a href="contato.html">Fale Conosco</a></li>
-                            </ul>
-                        </nav>
+                    <nav className="main-nav">
+                        <ul>
+                            <li>
+                                <Link to="/">Início</Link>
+                            </li>
+                            <li>
+                                <Link to="/product" className="active">
+                                    Produto
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/beneficios">Benefícios</Link>
+                            </li>
+                            <li>
+                                <Link to="/sobre">Sobre Nós</Link>
+                            </li>
+                            <li>
+                                <Link to="/contato">Fale Conosco</Link>
+                            </li>
+                        </ul>
+                    </nav>
 
-                        <div class="header-actions">
-                            <a href="#" class="btn btn-primary btn-mobile">Solicitar Demo</a>
-                            <button class="menu-toggle" id="menuToggle">
-                                <i class="fas fa-bars"></i>
-                            </button>
-                        </div>
+                    <div className="header-actions">
+                        <Link to="/demo" className="btn btn-primary btn-mobile">
+                            Solicitar Demo
+                        </Link>
+                        <button className="menu-toggle" id="menuToggle">
+                            <i className="fas fa-bars"></i>
+                        </button>
                     </div>
                 </div>
-            </header>
-        </>
+            </div>
+        </header>
     );
 }
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "../pages/Product/Product";
 import MainLayout from "../layouts/MainLayout";
+import Beneficios from "../pages/Beneficios/Beneficios";
 
 export default function AppRoutes() {
     return (
@@ -8,6 +9,7 @@ export default function AppRoutes() {
             <MainLayout>
                 <Routes>
                     <Route path="/product" element={<Product />} />
+                    <Route path="/beneficios" element={<Beneficios />} />
                 </Routes>
             </MainLayout>
         </BrowserRouter>
