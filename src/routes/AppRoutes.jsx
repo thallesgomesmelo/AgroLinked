4aab1adae4997e4779772a9b0ac546bccde58cb5
Mário_Lvm <46358711+maluter213@users.noreply.github.com>
2025-08-20 14:44:31 -0300

    start
    pagina inicio

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import Product from "../pages/Product/Product";
 import TalkUs from "../pages/TalkUs/TalkUs";
 import MainLayout from "../layouts/MainLayout";
 import AboutUs from "../pages/AboutUs/AboutUs";
+import Start from "../pages/start/start";
 
 function NotFound() {
   return (
@@ -20,17 +21,16 @@ function NotFound() {
 export default function AppRoutes() {
   return (
     <BrowserRouter>
-      <MainLayout>
-        <Routes>
-          <Route path="/" element={<AboutUs />} />
-
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/talkus" element={<TalkUs />} />
-
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route index element={<Start />} />
+          <Route path="start" element={<Start />} />
+          <Route path="about" element={<AboutUs />} />
+          <Route path="product" element={<Product />} />
+          <Route path="talkus" element={<TalkUs />} />
           <Route path="*" element={<NotFound />} />
-        </Routes>
-      </MainLayout>
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
