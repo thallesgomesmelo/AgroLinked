7a3b9fd8e30b0afe406b6e3353a8dd135004b219
Thalles Melo <thalles.melo@mestria.com.br>
2025-08-27 16:26:37 -0300

    feat(WeatherDashboard): Add Open-Meteo API.

--- src/pages/WeatherDashboard/WeatherDashboard.jsx
+++ src/pages/WeatherDashboard/WeatherDashboard.jsx
@@ -1,35 +1,8 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./WeatherDashboard.css"
 
-import { Sun, Cloud, CloudRain, Zap, CloudSnow, Droplets, Wind, Gauge, Search } from "lucide-react"
-
-const weatherData = {
-  location: "Cortes, Madrid, Spain",
-  date: "Terça-feira, 3:00 PM",
-  temperature: 49,
-  metrics: {
-    aqi: 67,
-    humidity: 41,
-    precipitation: 7,
-    wind: 23,
-  },
-  aqiDetails: {
-    value: 120,
-    status: "Ruim",
-    description:
-      "O ar atingiu um alto nível de poluição e não é saudável para grupos sensíveis. Reduza o tempo gasto ao ar livre se estiver sentindo sintomas como dificuldade para respirar ou irritação na garganta",
-  },
-  forecast: [
-    { day: "Sex", date: 27, icon: Sun, temp: 49, low: 28 },
-    { day: "Sáb", date: 27, icon: Cloud, temp: 49, low: 28 },
-    { day: "Dom", date: 27, icon: Zap, temp: 49, low: 28 },
-    { day: "Seg", date: 27, icon: Sun, temp: 49, low: 28 },
-    { day: "Ter", date: 27, icon: Sun, temp: 49, low: 28 },
-    { day: "Qua", date: 27, icon: Sun, temp: 49, low: 28 },
-    { day: "Qui", date: 27, icon: CloudSnow, temp: 49, low: 28 },
-  ],
-}
-
+import { Sun, CloudRain, CloudSun, Droplets, Wind, Gauge, Search } from "lucide-react"
+import { fetchWeatherApi } from "openmeteo"
 
 const tabs = [
   { id: "temperature", label: "Temperatura", icon: Sun },
@@ -39,15 +12,42 @@ const tabs = [
 ]
 
 export default function WeatherDashboard() {
-
   const [activeTab, setActiveTab] = useState("aqi")
   const [searchCity, setSearchCity] = useState("")
-  const [currentCity, setCurrentCity] = useState("Cortes, Madrid, Espanha")
+  const [currentCity, setCurrentCity] = useState("São Paulo, São Paulo")
+  const [weatherData, setWeatherData] = useState({
+    current: {
+      time: "",
+      temperature: "",
+      humidity: "",
+      precipitation: "",
+      rain: "",
+      wind_speed: "",
+      aqi: "",
+    },
+    forecast: {
+      day: [],
+      temp_max: "",
+      temp_min: "",
+      precipitation: "",
+      wind_speed: "",
+    },
+    aqiDetails: {
+      value: "",
+      status: "",
+      description:
+        "",
+    },
+  })
 
-  const handleSearch = (e) => {
+  const handleSearch = async (e) => {
     e.preventDefault()
+
     if (searchCity.trim()) {
-      setCurrentCity(searchCity.trim())
+      const { location, state, latitude, longitude } = await geoCoding(searchCity.trim())
+      weather(latitude, longitude)
+
+      setCurrentCity(`${location}, ${state}`)
       setSearchCity("")
     }
   }
@@ -55,6 +55,86 @@ export default function WeatherDashboard() {
   const getAQIPosition = (value) => {
     return Math.min((value / 400) * 100, 100)
   }
+
+  const url = "https://api.open-meteo.com/v1/forecast";
+
+  const weather = async (latitude, longitude) => {
+    const params = {
+      "latitude": latitude,
+      "longitude": longitude,
+      "daily": ["temperature_2m_max", "temperature_2m_min", "precipitation_probability_max", "wind_speed_10m_max", "rain_sum"],
+      "current": ["temperature_2m", "relative_humidity_2m", "precipitation", "rain", "wind_speed_10m"],
+      "timezone": "America/Sao_Paulo",
+    };
+
+    const responses = await fetchWeatherApi(url, params);
+    const response = responses[0]
+
+    const utcOffsetSeconds = response.utcOffsetSeconds();
+    const current = response.current();
+    const daily = response.daily();
+
+    const dateTime = () => {
+      const date = new Date((Number(current.time()) + utcOffsetSeconds) * 1000)
+      return new Intl.DateTimeFormat("pt-BR", { weekday: "long", hour: "2-digit", hour12: true }).format(date)
+    }
+
+    setWeatherData({
+      current: {
+        time: dateTime(),
+        temperature: parseFloat(current.variables(0).value()).toFixed(1),
+        humidity: parseFloat(current.variables(1).value()).toFixed(1),
+        precipitation: parseFloat(current.variables(2).value()).toFixed(1),
+        rain: parseFloat(current.variables(3).value()).toFixed(1),
+        wind_speed: parseFloat(current.variables(4).value()).toFixed(1),
+        aqi: 67,
+      },
+      forecast: {
+        day: [...Array((Number(daily.timeEnd()) - Number(daily.time())) / daily.interval())].map(
+          (_, i) => {
+            const date = new Date((Number(daily.time()) + i * daily.interval() + utcOffsetSeconds) * 1000)
+            return new Intl.DateTimeFormat("pt-BR", { weekday: 'short', day: "2-digit" }).format(date).replace(".", "")
+          }
+        ),
+        temp_max: daily.variables(0).valuesArray(),
+        temp_min: daily.variables(1).valuesArray(),
+        precipitation: daily.variables(2).valuesArray(),
+        wind_speed: daily.variables(3).valuesArray(),
+        rain: daily.variables(4).valuesArray(),
+      },
+      aqiDetails: {
+        value: 120,
+        status: "Ruim",
+        description:
+          "O ar atingiu um alto nível de poluição e não é saudável para grupos sensíveis. Reduza o tempo gasto ao ar livre se estiver sentindo sintomas como dificuldade para respirar ou irritação na garganta",
+      },
+    })
+  }
+
+  const geoCoding = async (city) => {
+    try {
+      const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}+&count=1&language=en&format=json&countryCode=BR`)
+
+      if (!response.ok) {
+        throw new Error("Erro ao buscar dados");
+      }
+
+      const data = await response.json()
+
+      const state = data.results[0].admin1
+      const location = data.results[0].name
+      const latitude = data.results[0].latitude
+      const longitude = data.results[0].longitude
+
+      return { location, state, latitude, longitude }
+    } catch (error) {
+      alert("Não foi possivel realizar pesquisa por cidade.")
+    }
+  }
+
+  useEffect(() => {
+    weather(-23.5475, -46.63611)
+  }, [])
 
   return (
     <div className="weather-container">
@@ -83,34 +163,39 @@ export default function WeatherDashboard() {
           {/* Header */}
           <div className="weather-header">
             <div className="weather-location">
-              <h1>{weatherData.location}</h1>
-              <p>{weatherData.date}</p>
+              <h1>{currentCity}</h1>
+              <p>{weatherData.current.time}</p>
             </div>
             <div className="weather-metrics">
               <div className="metric-item">
                 <Gauge className="metric-icon" />
-                <span>IQA: {weatherData.metrics.aqi}</span>
+                <span>IQA: {weatherData.current.aqi}</span>
               </div>
               <div className="metric-item">
                 <Droplets className="metric-icon" />
-                <span>Umidade: {weatherData.metrics.humidity}%</span>
+                <span>Umidade: {weatherData.current.humidity}%</span>
               </div>
               <div className="metric-item">
                 <CloudRain className="metric-icon" />
-                <span>Precipitação: {weatherData.metrics.precipitation}%</span>
+                <span>Precipitação: {weatherData.current.precipitation}%</span>
               </div>
               <div className="metric-item">
                 <Wind className="metric-icon" />
-                <span>Vento: {weatherData.metrics.wind} mph</span>
+                <span>Vento: {weatherData.current.wind_speed} mph</span>
               </div>
             </div>
           </div>
 
           {/* Current Weather */}
           <div className="current-weather">
-            <Sun className="weather-icon" />
+            {weatherData.current.precipitation > 0.2 ? (
+              <CloudRain className="weather-icon cloudRain" />
+            ) : (
+              <Sun className="weather-icon" />
+            )
+            }
             <div className="temperature-display">
-              <span className="temperature-main">{weatherData.temperature}</span>
+              <span className="temperature-main">{weatherData.current.temperature}</span>
               <div className="temperature-units">
                 <span className="celsius">°C</span>
               </div>
@@ -168,17 +253,30 @@ export default function WeatherDashboard() {
 
           {/* 7-Day Forecast */}
           <div className="forecast-grid">
-            {weatherData.forecast.map((day, index) => {
-              const Icon = day.icon
+            {weatherData.forecast.day.map((day, index) => {
+              const temp_max = parseFloat(weatherData.forecast.temp_max[index]).toFixed(1)
+              const temp_min = parseFloat(weatherData.forecast.temp_min[index]).toFixed(1)
+
+              let Icon = Sun
+              let styleIcon = "sun"
+
+              if (weatherData.forecast.precipitation[index] > 0.2) {
+                Icon = CloudRain
+                styleIcon = "cloud"
+              } else if (weatherData.forecast.temp_max[index] < 20) {
+                Icon = CloudSun
+                styleIcon = "cloudSun"
+              }
+
               return (
                 <div key={index} className="forecast-day">
                   <div className="forecast-date">
-                    {day.day} {day.date}
+                    {day}
                   </div>
-                  <Icon className="forecast-icon" />
+                  <Icon className={`forecast-icon ${styleIcon}`} />
                   <div className="forecast-temps">
-                    <div className="forecast-temp-high">{day.temp}°</div>
-                    <div className="forecast-temp-low">{day.low}°</div>
+                    <div className="forecast-temp-high">{temp_max}°</div>
+                    <div className="forecast-temp-low">{temp_min}°</div>
                   </div>
                 </div>
               )
